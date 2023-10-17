90f4a2b91ceabb9ec4a088068f3799d61ee823bf
Solomiia Lypynska <yaremkosolomiya@gmail.com>
2023-10-17 23:21:42 +0300

    first commit

---
+++ src/main/java/com/myresume/api/vacanci/MyResumeApiVacanci/MyResumeApiVacanciApplication.java
@@ -0,0 +1,13 @@
+package com.myresume.api.vacanci.MyResumeApiVacanci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyResumeApiVacanciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyResumeApiVacanciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myresume/api/vacanci/MyResumeApiVacanci/MyResumeApiVacanciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myresume.api.vacanci.MyResumeApiVacanci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyResumeApiVacanciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
